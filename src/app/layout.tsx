6e7edea1dd6fac0f5eebe32049dbe548aaa3b096
intelli-ia <intelliai.projetos@gmail.com>
2026-07-22 10:37:54 -0300

    feat: integrate Google Tag Manager into the main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Alegreya_Sans } from "next/font/google";
+import Script from "next/script";
 
 const alegreyaSansHeading = Alegreya_Sans({
   subsets: ["latin"],
@@ -35,7 +36,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5CD92TFC');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className={`${alegreyaSansHeading.variable} ${alegreyaSansBody.variable} font-body antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5CD92TFC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         <main>{children}</main>
         <Footer />
